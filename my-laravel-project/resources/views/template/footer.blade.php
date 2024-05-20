7ea721f99c27989cba633a06b6c2c5d569407365
Manish Sangroula <40350103+sseujs@users.noreply.github.com>
2024-05-20 10:14:06 +0000

    h

---
+++ my-laravel-project/resources/views/template/footer.blade.php
@@ -0,0 +1,67 @@
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+
+<div class="container">
+  <div class="row gy-4">
+    <div class="col-lg-5 col-md-12 footer-info">
+      <a href="index.html" class="logo d-flex align-items-center">
+        <span>KIG</span>
+      </a>
+      <p id="about" style="text-align: justify;">
+        Welcome to our digital haven! We are a dedicated team of web developers on a mission to craft exceptional online experiences. With a passion for innovation and attention to detail, we specialize in creating dynamic and user-friendly websites that surpass expectations. Join us in transforming ideas into captivating online realities, and let's embark on this exciting journey together.</p>
+      <div class="social-links d-flex mt-4">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+      </div>
+    </div>
+
+    <div class="col-lg-2 col-6 footer-links">
+      <h4>Useful Links</h4>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Utilities</a></li>
+        <li><a href="#">Terms of service</a></li>
+        <li><a href="#">Privacy policy</a></li>
+      </ul>
+    </div>
+
+    <div class="col-lg-2 col-6 footer-links">
+      <h4>Our Services</h4>
+      <ul>
+        <li><a href="#">Web Design</a></li>
+        <li><a href="#">Web Development</a></li>
+        <li><a href="#">Product Management</a></li>
+        <li><a href="#">Marketing</a></li>
+        <li><a href="#">Search Engine Optimiztion</a></li>
+      </ul>
+    </div>
+
+    <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+      <h4>Contact Us</h4>
+      <p>
+        Shivasatakshi-10, Maidhar <br>
+        Koshi Province, Jhapa<br>
+        Nepal <br><br>
+        <strong>Phone:</strong> +977 9816039599<br>
+        <strong>Email:</strong> manishsangroula21@gmail.com<br>
+      </p>
+
+    </div>
+
+  </div>
+</div>
+
+<div class="container mt-4">
+  <div class="copyright">
+    &copy; Copyright <strong><span>KIG</span></strong>. All Rights Reserved
+  </div>
+  <div class="credits">
+    Designed by <a href="https://manishsangroula.com.np/">Manish Sangroula</a>
+  </div>
+</div>
+
+</footer><!-- End Footer -->
+<!-- End Footer -->
